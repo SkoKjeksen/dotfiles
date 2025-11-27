75b276ef273778aabae0e502a90dcad2c8a6b63a
Emil Nilsen <enilsen@akvagroup.com>
2025-11-27 18:11:04 +0100

    disables arrow keys in nvim h3h3h3h

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,13 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-local M = {}
 
-M.general = {
-    n = {
-        ["<C-h>"] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
-        ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right" },
-        ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down" },
-        ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up" },
-    },
-}
+-- Disable arrow keys in normal mode (force hjkl)
+vim.keymap.set("n", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("n", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+-- Disable arrow keys in insert mode (hardcore mode)
+vim.keymap.set("i", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("i", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+-- Disable arrow keys in visual mode
+vim.keymap.set("v", "<Up>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Down>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Left>", "<Nop>", { noremap = true, silent = true })
+vim.keymap.set("v", "<Right>", "<Nop>", { noremap = true, silent = true })
