91845bd0c254fee732718fd8d055c2eb3836d17d
Emil Nilsen <Emil_n@live.no>
2025-11-29 18:12:54 +0100

    fixed autosave in lazyvim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,5 +1,5 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- autocmds are automatically loaded on the verylazy event
+-- default autocmds that are always set: https://github.com/lazyvim/lazyvim/blob/main/lua/lazyvim/config/autocmds.lua
 --
 -- Add any additional autocmds here
 -- with `vim.api.nvim_create_autocmd`
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Save the current buffer before switching to a tmux pane
+vim.g.tmux_navigator_save_on_switch = 2
