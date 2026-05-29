81e5cb3d283543d3f56b47d400de0e1cca0d205c
Emil <enilsen@akvagroup.com>
2026-05-29 12:48:53 +0200

    added telescope

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -16,4 +16,19 @@ return {
             { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
         },
     },
+    { -- TELESCOPE ---
+      "nvim-telescope/telescope.nvim",
+      tag= "0.1.4",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      config = function()
+        local telescope = require("telescope")
+        telescope.setup({})
+        local b = require("telescope.builtin")
+        vim.keymap.set('n', '<leader>ff', b.find_files, { desc = "Find Files"})
+        vim.keymap.set('n', '<leader>fg', b.live_grep, { desc = "Live Grep"})
+        vim.keymap.set('n', '<leader>fb', b.buffers, { desc = "Find Buffers"})
+        vim.keymap.set('n', '<leader>fh', b.help_tags, { desc = "Find Help"})
+    end
+
+  }
 }
