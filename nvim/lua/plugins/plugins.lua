de9a0f6f47566eda48e5588447f7b824c67fb24c
Emil Nilsen <enilsen@akvagroup.com>
2026-09-01 15:35:00 +0200

    added telescope filebrowser

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -19,10 +19,21 @@ return {
     { -- TELESCOPE ---
       "nvim-telescope/telescope.nvim",
       tag= "0.1.4",
-      dependencies = { "nvim-lua/plenary.nvim" },
+      dependencies = { "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "nvim-telescope/telescope-file-browser.nvim" },
       config = function()
         local telescope = require("telescope")
-        telescope.setup({})
+        telescope.setup({
+        extensions = {
+          file_browser = {
+            hidden = true,
+            grouped = true,
+            respect_gitignore = false,
+            previewer = false,
+            initial_mode = "normal",
+          },
+        },
+      })
+      telescope.load_extension("file_browser")
         local b = require("telescope.builtin")
         vim.keymap.set('n', '<leader>ff', b.find_files, { desc = "Find Files"})
         vim.keymap.set('n', '<leader>fg', b.live_grep, { desc = "Live Grep"})
